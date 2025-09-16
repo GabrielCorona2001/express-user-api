a2ff528519fe561a0fb28d46740d4ff76ad3c750
Gabriel Corona <gabrielcoronanunes2001@gmail.com>
2025-09-15 22:53:15 -0300

    chore: config prettier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log("rodou no", PORT)
-})
+  console.log("rodou no", PORT);
+});
